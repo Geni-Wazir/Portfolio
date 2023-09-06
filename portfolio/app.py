04649bd1240c8da07917354f753d0c4ceb4bbbfd
Geni-Wazir <crazycoolbrother@gmail.com>
2023-09-06 20:41:16 +0530

    Updated for deployment

--- portfolio/app.py
+++ portfolio/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, render_template, url_for, redirect
+from flask import Flask, render_template
 import feedparser
 
 
@@ -49,4 +49,4 @@ def blogs():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
